d919e802730d36da49ba9cf836aaf3ba036d2841
samuelfgs96 <samuel.fg96@gmail.com>
2018-06-13 23:08:00 -0300

    inicio

---
+++ README.md
@@ -0,0 +1,3 @@
+Transformação de imagem em código
+
+O objetivo é transformar as imagens em código, para isso, será utilizado técnicas de realce para melhorar as imagens. Além disso, as imagens do código podem vir separadas em algumas imagens, para isso serão utilizado algumas técnicas para juntar as imagens e depois transformá-la num único código.
---
+++ teste.py
@@ -0,0 +1,13 @@
+from tesserocr import PyTessBaseAPI
+
+images = ['images/img01.jpg', 'images/t1.png', 'images/t2.png']
+
+with PyTessBaseAPI() as api:
+    for img in images:
+        print (img)
+        print ('###################################################')
+        api.SetImageFile(img)
+        print (api.GetUTF8Text())
+        print (api.AllWordConfidences())
+        print('\n\n\n')
+        print ('###################################################')
