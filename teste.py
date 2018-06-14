c2a295f4a474ad5b29e5e2e1d9e293fd6330fc1f
samuelfgs96 <samuel.fg96@gmail.com>
2018-06-13 23:19:12 -0300

    images t3.png

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 from tesserocr import PyTessBaseAPI
 
-images = ['images/img01.jpg', 'images/t1.png', 'images/t2.png']
+images = ['images/img01.jpg', 'images/t1.png', 'images/t2.png', 'images/t3.png']
 
 with PyTessBaseAPI() as api:
     for img in images:
